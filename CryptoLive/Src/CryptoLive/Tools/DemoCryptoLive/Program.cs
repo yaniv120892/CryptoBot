51553e89a2fa317747b128987d76ee389b7b3a03
Yaniv Daye <Yaniv.Daye@zerto.com>
2021-02-12 14:36:06 +0200

    remove unused using

--- CryptoLive/Src/CryptoLive/Tools/DemoCryptoLive/Program.cs
+++ CryptoLive/Src/CryptoLive/Tools/DemoCryptoLive/Program.cs
@@ -14,7 +14,6 @@ using Infra.NotificationServices;
 using Microsoft.Extensions.Logging;
 using Services.Abstractions;
 using Storage;
-using Storage.Abstractions;
 using Storage.Abstractions.Providers;
 using Storage.Abstractions.Repository;
 using Storage.Providers;
