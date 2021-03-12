624d1b6df8931e859b203d00688b561b84e4e7ef
Yaniv Daye <yaniv120892@gmail.com>
2021-03-12 16:12:36 +0200

    Improve GetLowerRsiAndHigherPrice constraints

--- CryptoLive/Src/CryptoLive/Common/CryptoQueue/CryptoFixedSizeQueueImpl.cs
+++ CryptoLive/Src/CryptoLive/Common/CryptoQueue/CryptoFixedSizeQueueImpl.cs
@@ -4,15 +4,17 @@ using Common.Abstractions;
 
 namespace Common.CryptoQueue
 {
-    public class CryptoFixedSizeQueueImpl<TPriceAndRsi> : ICryptoPriceAndRsiQueue<TPriceAndRsi>  where TPriceAndRsi: PriceAndRsi
+    public class CryptoFixedSizeQueueImpl<TPriceAndRsi> : ICryptoPriceAndRsiQueue<TPriceAndRsi>
+        where TPriceAndRsi : PriceAndRsi
     {
+        private static readonly decimal s_factor = (decimal)1.02;
         private readonly FixedSizeQueue<TPriceAndRsi> m_fixedSizeQueue;
-        
+
         public CryptoFixedSizeQueueImpl(int size)
         {
             m_fixedSizeQueue = new FixedSizeQueue<TPriceAndRsi>(size);
         }
-        
+
         public void Enqueue(TPriceAndRsi currentPriceAndRsi)
         {
             m_fixedSizeQueue.Enqueue(currentPriceAndRsi);
@@ -20,12 +22,13 @@ namespace Common.CryptoQueue
 
         public TPriceAndRsi GetLowerRsiAndHigherPrice(TPriceAndRsi priceAndRsi)
         {
-            return m_fixedSizeQueue.MyQueue.FirstOrDefault(oldRsiAndPrice => 
-                priceAndRsi.Rsi > oldRsiAndPrice.Rsi
-                && priceAndRsi.Price < oldRsiAndPrice.Price
-                //&& priceAndRsi.Price * (decimal) 1.02 < oldRsiAndPrice.Price
-                 && oldRsiAndPrice.CandleTime < priceAndRsi.CandleTime.Subtract(TimeSpan.FromMinutes(30))
+            return m_fixedSizeQueue.MyQueue.FirstOrDefault(oldRsiAndPrice =>
+                IsLowerRsiAndHigherPrice(oldRsiAndPrice, priceAndRsi)
+                && oldRsiAndPrice.CandleTime < priceAndRsi.CandleTime.Subtract(TimeSpan.FromMinutes(30))
             );
         }
+
+        private static bool IsLowerRsiAndHigherPrice(TPriceAndRsi oldRsiAndPrice, TPriceAndRsi newPriceAndRsi) =>
+           newPriceAndRsi.Rsi > oldRsiAndPrice.Rsi && newPriceAndRsi.Price * s_factor < oldRsiAndPrice.Price;
     }
 }
