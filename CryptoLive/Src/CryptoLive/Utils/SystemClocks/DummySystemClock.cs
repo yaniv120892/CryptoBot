1455ac5c0557a4127bf54695179de348c12f1baa
Yaniv Daye <yaniv120892@gmail.com>
2021-03-13 22:21:22 +0200

    DummySystemClock - validate no cancellation request

--- CryptoLive/Src/CryptoLive/Utils/SystemClocks/DummySystemClock.cs
+++ CryptoLive/Src/CryptoLive/Utils/SystemClocks/DummySystemClock.cs
@@ -11,12 +11,13 @@ namespace Utils.SystemClocks
     {
         private static readonly ILogger s_logger = ApplicationLogging.CreateLogger<DummySystemClock>();
 
-        public async Task<DateTime> Wait(CancellationToken cancellationToken, string currency, int timeToWaitInSeconds, string action,
+        public Task<DateTime> Wait(CancellationToken cancellationToken, string currency, int timeToWaitInSeconds, string action,
             DateTime currentTime)
         {
+            cancellationToken.ThrowIfCancellationRequested();
             //await Task.Delay(1 * timeToWaitInSeconds , cancellationToken);
             s_logger.LogTrace($"{currency}_{action}: dummy wait for next candle {timeToWaitInSeconds} seconds");
-            return currentTime.AddSeconds(timeToWaitInSeconds);
+            return Task.FromResult(currentTime.AddSeconds(timeToWaitInSeconds));
         }
     }
 }
