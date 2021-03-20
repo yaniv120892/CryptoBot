e2fb5d05bfde05c0a8ad9a76de6b8df56de578fc
yaniv120892 <yaniv120892@gmail.com>
2021-03-20 22:32:15 +0200

    Fix bug - Filter LotSize error - align quantity with currency step size

--- CryptoLive/Src/CryptoLive/Services/BinanceTradeService.cs
+++ CryptoLive/Src/CryptoLive/Services/BinanceTradeService.cs
@@ -18,12 +18,14 @@ namespace Services
         private static readonly ILogger s_logger = ApplicationLogging.CreateLogger<BinanceTradeService>();
 
         private readonly Dictionary<string, decimal> m_symbolToTickSizesMapping;
+        private readonly Dictionary<string, decimal> m_symbolToStepSizesMapping;
         private readonly ICurrencyClientFactory m_currencyClientFactory;
 
         public BinanceTradeService(ICurrencyClientFactory currencyClientFactory)
         {
             m_currencyClientFactory = currencyClientFactory;
             m_symbolToTickSizesMapping = new Dictionary<string, decimal>();
+            m_symbolToStepSizesMapping = new Dictionary<string, decimal>();
         }
 
         public async Task<(decimal buyPrice, decimal quantity)> PlaceBuyMarketOrderAsync(string currency,
@@ -43,9 +45,10 @@ namespace Services
         {
             decimal priceByTickSize = await GetPriceAlignedToTickSizeAsync(currency, price);
             decimal stopAndLimitPriceByTickSize = await GetPriceAlignedToTickSizeAsync(currency, stopAndLimitPrice);
+            decimal quantityByStepSizes = await GetQuantityAlignedToStepSizeAsync(currency, quantity);
             var action = new Func<Task<WebCallResult<BinanceOrderOcoList>>>(async () =>
-                await PlaceSellOcoOrderImplAsync(currency, quantity, priceByTickSize, stopAndLimitPriceByTickSize));
-            string actionDescription = $"place OCO sell {quantity} {currency}, " +
+                await PlaceSellOcoOrderImplAsync(currency, quantityByStepSizes, priceByTickSize, stopAndLimitPriceByTickSize));
+            string actionDescription = $"place OCO sell {quantityByStepSizes} {currency}, " +
                                        $"price {priceByTickSize}, StopAndLimit: {stopAndLimitPriceByTickSize}";
             _ = await ExecuteTradeAndAssert(action, actionDescription);
         }
@@ -98,6 +101,12 @@ namespace Services
             decimal tickSize = await GetTickSizesAsync(currency);
             return (int)(price / tickSize) * tickSize;
         }
+        
+        private async ValueTask<decimal> GetQuantityAlignedToStepSizeAsync(string currency, decimal quantity)
+        {
+            decimal stepSizes = await GetStepSizesAsync(currency);
+            return (int)(quantity / stepSizes) * stepSizes;
+        }
 
         private static (decimal buyPrice, decimal quantity) ExtractPriceAndFilledQuantity(CallResult<BinancePlacedOrder> response)
         {  
@@ -106,7 +115,7 @@ namespace Services
             decimal paidAmount = 0;
             foreach (var binanceOrderTrade in binanceOrderTrades)
             {
-                quantityAmount += binanceOrderTrade.Quantity;
+                quantityAmount = quantityAmount + binanceOrderTrade.Quantity - binanceOrderTrade.Commission;
                 paidAmount += binanceOrderTrade.Price * binanceOrderTrade.Quantity;
             }
 
@@ -128,6 +137,21 @@ namespace Services
 
             return tickSizes;
         }
+        
+        private async ValueTask<decimal> GetStepSizesAsync(string currency)
+        {
+            if (!m_symbolToStepSizesMapping.TryGetValue(currency, out decimal stepSizes))
+            {
+                BinanceClient client = m_currencyClientFactory.Create();
+                var response = await client.Spot.System.GetExchangeInfoAsync();
+                ResponseHandler.AssertSuccessResponse(response, "GetExchangeInfo");
+                BinanceSymbol binanceSymbol = ExtractBinanceSymbol(currency, response);
+                stepSizes = ExtractStepSizes(binanceSymbol);
+                m_symbolToStepSizesMapping[currency] = stepSizes;
+            }
+
+            return stepSizes;
+        }
 
         private static decimal ExtractTickSizes(BinanceSymbol binanceSymbol)
         {
@@ -138,6 +162,17 @@ namespace Services
             }
 
             return ((BinanceSymbolPriceFilter) priceFilter).TickSize;
+        }
+        
+        private static decimal ExtractStepSizes(BinanceSymbol binanceSymbol)
+        {
+            BinanceSymbolFilter lotSizeFilter = binanceSymbol.Filters.SingleOrDefault(m => m.FilterType== SymbolFilterType.LotSize);
+            if (lotSizeFilter is null)
+            {
+                throw new Exception($"{binanceSymbol.Name} exchange info doesn't include LotSizeFilter");
+            }
+
+            return ((BinanceSymbolLotSizeFilter) lotSizeFilter).StepSize;
         }
 
         private static BinanceSymbol ExtractBinanceSymbol(string currency, CallResult<BinanceExchangeInfo> response)
