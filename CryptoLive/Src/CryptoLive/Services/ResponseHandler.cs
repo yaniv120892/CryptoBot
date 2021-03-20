29ee54236836917624c3c2f93062176bac49e676
yaniv120892 <yaniv120892@gmail.com>
2021-03-20 18:33:54 +0200

    Log more info when get error response

--- CryptoLive/Src/CryptoLive/Services/ResponseHandler.cs
+++ CryptoLive/Src/CryptoLive/Services/ResponseHandler.cs
@@ -1,4 +1,7 @@
+#nullable enable
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using CryptoExchange.Net.Objects;
 
 namespace Services
@@ -15,11 +18,26 @@ namespace Services
             if (!(response.Error is null))
             {
                 throw new Exception(
-                    $"{requestDescription}: Response returned with error, Message: {response.Error.Message}, Code: {response.Error.Code}, Data: {response.Error.Data}");
+                    $"{requestDescription}: Response returned with error, " +
+                    $"Message: {response.Error}, " +
+                    $"Code: {response.Error.Code}, " +
+                    $"Response status code: {response.ResponseStatusCode}, " +
+                    $"Data: {response.Error.Data}, " +
+                    $"ResponseHeader: {string.Join(",", GetResponseHeaderDescriptions(response.ResponseHeaders))}");
             }
 
             throw new Exception(
                 $"{requestDescription}: Response not success and has no Error, StatusCode: {response.ResponseStatusCode}");
+        }
+
+        private static List<string> GetResponseHeaderDescriptions(IEnumerable<KeyValuePair<string, IEnumerable<string>>>? responseResponseHeaders)
+        {
+            if (responseResponseHeaders is null)
+            {
+                return new List<string>();
+            }
+
+            return responseResponseHeaders.Select(m => $"[{m.Key}]={m.Value}").ToList();
         }
     }
 }
